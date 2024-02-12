e88f0807fb0dec37f7082a556527aff33b169da0
Hamza Ijaz <msalman.zafar@systemsltd.com>
2024-02-12 22:02:00 +0500

    project init

---
+++ BuberDinner.Application/Services/Authentication/IAuthenticationCommandService.cs
@@ -0,0 +1,15 @@
+﻿using FluentResults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuberDinner.Application.Services.Authentication
+{
+    public interface IAuthenticationCommandService
+    {
+        Task<Result<AuthenticationResult>> RegisterAsync(string firstName, string lastName, string email, string password);
+        Task<AuthenticationResult> LoginAsync(string email, string password);
+    }
+}
---
+++ BuberDinner.Domain/Entities/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuberDinner.Domain.Entities
+{
+    public class User
+    {
+        public User(string firstName, string lastName, string email, string password)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Password = password;
+        }
+
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
